e33349a0734a6995efdde96ed6a922050d85bf45
Wen <wzhang87@hawk.iit.edu>
2021-07-18 09:16:04 -0700

    Init work for precision map

---
+++ app.py
@@ -0,0 +1,48 @@
+import io
+import json
+
+from flask import Flask, request, render_template, make_response
+
+from utils import extension_validation, process_data_for_output
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def form():
+    if request.method == 'POST':
+        f = request.files['data_file']
+        if not f:
+            context = {"error": True, "error_message": "No File"}
+        else:
+            file_check = extension_validation(f.filename)
+            if file_check:
+                stream = io.StringIO(f.stream.read().decode("utf-8-sig"), newline=None)
+                final_data, error = process_data_for_output(stream)
+                if error:
+                    context = {"error": True, "error_message": error}
+                else:
+                    context = {"output": json.dumps(final_data), "enable_output": True}
+            else:
+                context = {"error": True, "error_message": "Incorrect format"}
+    else:
+        context = dict()
+    return render_template("base.html", **context)
+
+
+@app.route('/sample', methods=["GET"])
+def get_sample_json():
+    si = io.StringIO()
+    try:
+        file = open("sample.json", "r").read()
+    except Exception as e:
+        file = '''{"reference":{"ref-temp":50,"ref-hum":50},"data":{"temp-1":{"humidity":[{"timestamp":"2007-04-05T22:12","data":45}],"thermometer":[{"timestamp":"2007-04-05T22:00","data":72.4}]},"temp-2":{"thermometer":[{"timestamp":"2007-04-05T22:12","data":69.4}]}}}'''
+    si.write(json.loads(json.dumps(file)))
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=sample.json"
+    output.headers["Content-type"] = "text/json"
+    return output
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ utils.py
@@ -0,0 +1,65 @@
+import json
+import statistics
+
+
+def get_precision(mean, average_room_temp):
+    pre = average_room_temp - mean
+    if pre < 0:
+        pre = -1 * pre
+    return pre
+
+
+def extension_validation(filename, ext: list = ["json"]) -> bool:
+    """
+    Validated the extension of files
+    :param filename:
+    :param ext: default json
+    :return: bool
+    """
+    files_split = filename.split(".")
+    ext_for_file = files_split[-1]
+    if ext_for_file in ext:
+        return True
+    return False
+
+
+def process_data_for_output(inputdata):
+    try:
+        formatted_data_objects = json.loads(inputdata.read())
+        reference = formatted_data_objects.get("reference")
+        information = formatted_data_objects.get("data")
+        rel_humidity_reading = reference.get("ref-hum")
+        ref_temp_reading = reference.get("ref-temp")
+        final_data = dict()
+        for name, objects in information.items():
+            for type, value in objects.items():
+                if type == 'thermometer':
+                    temp_data = [x['data'] for x in value]
+                    reading = statistics.mean(temp_data)
+                    if len(temp_data) >= 1:
+                        temp_data = temp_data + [0,0]
+                    stdev = statistics.stdev(temp_data)
+                    precision = get_precision(reading, ref_temp_reading)
+                    if precision <= 0.5 and stdev < 3:
+                        final_data[f"{name}_{type}"] = "ultra-precise"
+                    elif precision <= 0.5 and stdev < 5:
+                        final_data[f"{name}_{type}"] = "very precise"
+                    else:
+                        final_data[f"{name}_{type}"] = "precise"
+                elif type == 'humidity':
+                    percentage_diff_array = []
+                    for x in value:
+                        diff = rel_humidity_reading - x['data'] if rel_humidity_reading - x['data'] > 0 else x[
+                                                                                                                 'data'] - rel_humidity_reading
+                        percentage_diff = ((diff / rel_humidity_reading) * 100)
+                        percentage_diff_array.append(percentage_diff)
+                    percentage_diff_mean = statistics.mean(percentage_diff_array)
+                    if percentage_diff_mean > 1:
+                        final_data[f"{name}_{type}"] = "discard"
+                    else:
+                        final_data[f"{name}_{type}"] = "keep"
+                else:
+                    final_data[f"{name}_{type}"] = "Unable to identify"
+        return final_data, None
+    except Exception as e:
+        return None, e
